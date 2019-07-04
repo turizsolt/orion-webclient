ec70e3cabee7a7decc514059dabea161a9a51f29
Zsolt Turi <turi.zsolt.m@gmail.com>
2019-07-04 10:44:57 +0200

    feat: Todo added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {TodoLister} from "./components/TodoLister";
+import {TodoModel} from "./model/TodoModel";
+
+const list:TodoModel[] = [
+  {description: 'one dfsgdfg'},
+  {description: 'two sdfgdfg'},
+  {description: 'three lkljlkkl'},
+];
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <TodoLister list={list} />
+        </div>
+    );
 }
 
 export default App;
