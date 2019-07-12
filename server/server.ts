bf0094237b99ff0dfe8d62c7b382b7a9e1f4c05e
turizsolt <turi.zsolt.m@gmail.com>
2019-07-12 15:18:11 +0200

    feat: server > todos can be added/modified/listed via REST

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,44 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => res.send('Hello World!'));
+
+interface Todo {
+    id: string;
+    epic: string;
+    title: string;
+    description: string;
+    createdAt: Date;
+    doneAt: Date;
+}
+
+const todos:Todo[] = [];
+
+app.post('/todo', (req, res) => {
+    const todo:Todo = req.body as Todo;
+    todos.push(todo);
+    res.send(todo);
+});
+
+app.get('/todo', (req, res) => {
+    res.send(todos);
+});
+
+app.put('/todo', (req, res) => {
+    const todoIndex = todos.findIndex(x => x.id == req.body.id);
+    if (todoIndex !== -1) {
+        const todo = todos[todoIndex];
+        const newTodo = {...todo, ...req.body};
+        todos[todoIndex] = newTodo;
+        res.send(newTodo);
+    } else {
+        res.send([]);
+    }
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
