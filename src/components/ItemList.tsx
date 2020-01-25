8258a7a494d7c7465d211b6a73a407c35bc140fa
turizsolt <turi.zsolt.m@gmail.com>
2020-01-25 12:40:22 +0100

    feat: ItemAdder

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { RootState } from '../store';
 import { Item as ItemTsx } from './Item';
 import { ItemEditor } from './ItemEditor';
-import { Item } from '../store/state/Item';
-import { Change } from '../store/state/Change';
-import { createItem, createRelation } from '../store/actions';
-import { ActualIdGenerator } from '../idGenerator/ActualIdGenerator';
-
-const idGenerator = new ActualIdGenerator();
+import { ItemAdder } from './ItemAdder';
 
 export const ItemList: React.FC = () => {
   const { allIds: itemIds, byId: items } = useSelector(
@@ -18,43 +13,6 @@ export const ItemList: React.FC = () => {
     (state: RootState) => state.appReducer
   );
 
-  const dispatch = useDispatch();
-
-  const handleAddRandom = React.useCallback(() => {
-    const item: Item = {
-      id: idGenerator.generate(),
-      fields: {
-        title: 'New item',
-        createdAt: new Date().toISOString(),
-        description: '',
-        state: 'todo'
-      }
-    };
-
-    const change: Change = {
-      type: 'CreateItem',
-      id: idGenerator.generate(),
-      data: {
-        item
-      }
-    };
-
-    dispatch(createItem.started(change));
-  }, [dispatch]);
-
-  const handleAddRandomChild = React.useCallback(() => {
-    const change: Change = {
-      id: idGenerator.generate(),
-      type: 'CreateRelation',
-      data: {
-        parentId: itemIds[0],
-        childId: itemIds[1]
-      }
-    };
-
-    dispatch(createRelation.started(change));
-  }, [dispatch, itemIds]);
-
   return (
     <>
       <div>version: {version}</div>
@@ -63,8 +21,7 @@ export const ItemList: React.FC = () => {
           {itemIds.map(id => (
             <ItemTsx item={items[id]} key={id} />
           ))}
-          <button onClick={handleAddRandom}>Add random</button>
-          <button onClick={handleAddRandomChild}>Random child</button>
+          <ItemAdder />
         </div>
         <div>{selectedId && <ItemEditor itemId={selectedId} />}</div>
       </div>
