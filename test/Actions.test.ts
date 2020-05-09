bdc5e67d4c46c304360b3eaa1e02ee205bfc4c45
turizsolt <turi.zsolt.m@gmail.com>
2020-05-09 22:55:24 +0200

    fiddling: first test

--- test/Actions.test.ts
+++ test/Actions.test.ts
@@ -1,7 +1,18 @@
 import { expect } from 'chai';
 
 describe('Actions', () => {
-  it('first', () => {
+  it('create an item', () => {
+    const change = new Change({
+      itemId: '1234567890',
+      field: 'title',
+      oldValue: undefined,
+      newValue: 'Lorem Ipsum'
+    });
+    const transaction = new Transaction();
+    transaction.add(change);
+    const store = new Store();
+    store.commit(transaction);
+
     expect(true).equals(true);
   });
 });
