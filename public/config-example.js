7c1820dfe27aa6584b90989639f6eeff32bb4343
turizsolt <turi.zsolt.m@gmail.com>
2023-06-24 14:19:34 +0200

    fix: using reduxTools moved to config

--- public/config-example.js
+++ public/config-example.js
@@ -1,2 +1,3 @@
 var socketIoUrl = 'http://localhost:80/';
 var loginUrl = 'http://localhost:80/';
+var reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__; // set true to omit devtools 
--- src/ReduxStore/index.ts
+++ src/ReduxStore/index.ts
@@ -8,8 +8,8 @@ export type RootState = ReturnType<typeof rootReducer>;
 
 export const sagaMiddleware = createSagaMiddleware();
 
-declare var window: any;
-const composeEnhancers = /* window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || */ compose;
+declare var reduxDevTools : any;
+const composeEnhancers = reduxDevTools || compose;
 export const twoStore = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(sagaMiddleware))
